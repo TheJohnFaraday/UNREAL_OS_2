82d88bc846952e67a4728292903aed63d7572f4e
brufarITBA <bfarbiarz@gmail.com>
2023-05-26 12:21:13 -0300

    buddy

--- Kernel/include/MemoryManager.h
+++ Kernel/include/MemoryManager.h
@@ -5,8 +5,6 @@
 
 #define START_MEM 0x500000
 #define START_MEM_USERS 0x600000
-#define USED 1
-#define FREE 0
 
 
 typedef struct MemoryManagerCDT *MemoryManagerADT;
@@ -14,7 +12,5 @@ typedef struct MemoryManagerCDT *MemoryManagerADT;
 void createMemoryManager(void *const restrict memoryForMemoryManager, void *const restrict managedMemory);
 
 void *allocMemory(const size_t memoryToAllocate);
-
-void free(void *ptr);
 
 #endif
